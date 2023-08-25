1b104b1411d391e008e111bee9274901472a0d64
aditiyakamodiya <aditiyakamodiya@gmail.com>
2023-08-25 12:28:35 +0530

    without css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react'
 
 function App() {
+  let str = 'abcdERTPOFIDHCNXMALQvncmx'
+  const [save, setsave] = useState([])
+  const [val, setval] = useState([])
+  let temp = ''
+  
+
+   useEffect(()=>{
+    for (let i = 0; i < 6; i++) {
+      let random = str[Math.floor(Math.random() * str.length)]
+      temp = temp + random
+    }
+    setsave(temp)
+  },[]) 
+    // console.log(save)
+
+  function Refreshfunc(){
+    for (let i = 0; i < 6; i++) {
+      let random = str[Math.floor(Math.random() * str.length)]
+      temp = temp + random
+    }
+    setsave(temp)
+  }
+
+  
+  // generate();
+  // console.log(save.length)
+
+  function checkfunc() {
+    let j = 0;
+    // console.log('tes')
+    if (val != '' && save) {
+
+      // console.log(val)
+      for (let i = 0; i < save.length; i++) {
+
+        if (val[i] === save[i]) {
+          j++
+          if (j === save.length) {
+            console.log('valid')
+            console.log(j)
+
+          }
+
+
+        }
+
+
+      }
+
+    }
+    if(  val != ''&& j!=6){
+      console.log('invalid')
+    }
+
+    if(val == '') {
+      alert('empty')
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <p>{save}</p>
+      <input placeholder='copy this' value={val} onChange={(e) => setval(e.target.value)}></input>
+      <button onClick={checkfunc}>check </button>
+      <button onClick={Refreshfunc}>Refresh</button>
+
+    </>
+  )
 }
 
-export default App;
+export default App
